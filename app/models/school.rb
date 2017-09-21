cd675a06f5dba890f56f9ada1d0abce4794df2f1
gablooblue <gabrielhenry.lopez@gmail.com>
2017-09-21 21:11:23 +0800

    Fixed teacher and subject relations

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,3 +1,4 @@
 class School < ApplicationRecord
     has_many :subjects
+    has_many :teachers, through: :subjects
 end
--- db/migrate/20170919092720_create_teachers.rb
+++ db/migrate/20170919092720_create_teachers.rb
@@ -1,7 +1,6 @@
 class CreateTeachers < ActiveRecord::Migration[5.0]
   def change
     create_table :teachers do |t|
-      t.integer :subject_id
 
       t.timestamps
     end
