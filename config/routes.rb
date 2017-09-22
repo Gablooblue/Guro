3d71a0d35fca5f5c8045a48fdc4e925e41af5284
gablooblue <gabrielhenry.lopez@gmail.com>
2017-09-22 11:35:31 +0800

    Added admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :subjects
   resources :schools
 
+  resources :admin, only: :index
+
   root "splash#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
