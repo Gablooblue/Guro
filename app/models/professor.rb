6ed711268756a10259538d38d262027dd06cbd3b
gablooblue <gabrielhenry.lopez@gmail.com>
2017-09-23 23:10:15 +0800

    Added average ratings

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -3,4 +3,12 @@ class Professor < ApplicationRecord
     has_and_belongs_to_many :subjects
     has_many :schools, through: :subject
     has_many :reviews
+
+    def average_rating
+	self.reviews.average(:score).where(subject: self.subject)
+    end
+
+    def overall_rating
+	self.reviews.average(:score)
+    end
 end
