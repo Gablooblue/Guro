f68778688f87ca0b3de61070cc422439cc86f7fe
gablooblue <gabrielhenry.lopez@gmail.com>
2017-09-24 11:27:57 +0800

    Added functions for adding professors

--- app/controllers/subject_professors_controller.rb
+++ app/controllers/subject_professors_controller.rb
@@ -1,2 +1,22 @@
 class SubjectProfessorsController < ApplicationController
+
+    #Creating a subject-professor relationship
+    def create
+	@subject = Subject.find(params[:id])
+	@professor = Professor.find(params[:professor])
+
+	@subject << @professor
+    end
+
+    #Destroying a subject-professor relationship
+    def destroy
+	@subject = Subject.find(params[:subject][:id])
+	@professor = @subject.professors.find(params[:professor][:id])
+	
+	if @professor
+	    @subject.professors.delete(@professor)
+	end
+
+    end
+	
 end
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -63,6 +63,12 @@ class SubjectsController < ApplicationController
 	end
     end
 
+    def add_professor
+	@professor = Professor.find(params[:professor_id])
+
+	@subject << @professor
+    end
+
     private
     # Use callbacks to share common setup or constraints between actions.
     def set_subject
