1a6779ccbee283e91dad0319c9a8929c98a026af
gablooblue <gabrielhenry.lopez@gmail.com>
2017-09-25 11:02:57 +0800

    Added cover photos

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,6 +15,7 @@ class ReviewsController < ApplicationController
   # GET /reviews/new
   def new
     @review = Review.new
+    @professor = Professor.find(params[:professor_id])
   end
 
   # GET /reviews/1/edit
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( splash.scss )
 Rails.application.config.assets.precompile += %w( subjects.css )
+Rails.application.config.assets.precompile += %w( schools.scss )
